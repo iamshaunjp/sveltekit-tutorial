c30abcb4618b3ca3e139c2d1eb8af0ee436ef984
Shaun <shaun@thenetninja.co.uk>
2021-12-10 17:43:59 +0000

    lesson-13

---
+++ ninja-gaming/src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+	export function load({ error }) {
+		return {
+			props: {
+				message: error.message
+			}
+		};
+	}
+</script>
+
+<script>
+	export let message
+</script>
+
+<div class="error">
+  <h2>Oops</h2>
+  <p>{message}</p>
+  <a href="/guides">View all guides</a>
+</div>
+
+<style>
+  .error {
+    text-align: center;
+    margin-bottom: 60px;
+  }
+</style>
