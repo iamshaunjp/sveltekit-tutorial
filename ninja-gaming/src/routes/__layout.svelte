fe5bc9e9f4d8662ec15b1a4e052f21f4d64466ba
Shaun <shaun@thenetninja.co.uk>
2021-12-10 17:25:39 +0000

    lesson-6

---
+++ ninja-gaming/src/routes/__layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Title from '$lib/title.svelte'
+  import '../styles/global.css'
+</script>
+
+<header>
+  <Title title="Ninja Gaming Reviews" />
+</header>
+<main>
+  <slot></slot>
+</main>
+<footer>
+  <p>Copyright 2022 Ninja Gaming Reviews</p>
+</footer>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+  }
+  main {
+    max-width: 960px;
+    margin: 20px auto;
+  }
+  footer {
+    text-align: center;
+  }
+</style>
--- ninja-gaming/src/routes/about.svelte
+++ ninja-gaming/src/routes/about.svelte
@@ -2,7 +2,6 @@
   import Title from '$lib/title.svelte'
 </script>
 
-<Title title="Ninja Gaming Reviews" />
 <div class="about">
   <h2>About Us</h2>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt laborum magnam deleniti recusandae, quis aperiam, architecto neque, voluptatem dicta expedita aut id voluptate omnis consectetur dolore! Quo quae perferendis vel dignissimos quasi illum aperiam dolores, id, laudantium voluptatibus fugiat accusantium impedit velit modi ratione iure qui culpa! Repellendus cupiditate aliquid quam magni. Laudantium, laboriosam quibusdam. Provident, quae incidunt! Aspernatur, vel?</p>
