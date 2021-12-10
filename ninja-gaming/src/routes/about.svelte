0cc330507eb8f650643c7546a87562f0aaa358b8
Shaun <shaun@thenetninja.co.uk>
2021-12-10 16:49:47 +0000

    lesson-4

--- ninja-gaming/src/routes/about.svelte
+++ ninja-gaming/src/routes/about.svelte
@@ -1,16 +1,12 @@
-<script>
-
-</script>
-
-<div class="index">
-  <h2>Welcome</h2>
+<div class="about">
+  <h2>About Us</h2>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt laborum magnam deleniti recusandae, quis aperiam, architecto neque, voluptatem dicta expedita aut id voluptate omnis consectetur dolore! Quo quae perferendis vel dignissimos quasi illum aperiam dolores, id, laudantium voluptatibus fugiat accusantium impedit velit modi ratione iure qui culpa! Repellendus cupiditate aliquid quam magni. Laudantium, laboriosam quibusdam. Provident, quae incidunt! Aspernatur, vel?</p>
   <a href="/guides">View Guides</a>
-  <a href="/about">About</a>
+  <a href="/">Home</a>
 </div>
 
 <style>
-  .index {
+  .about {
     text-align: center;
     display: block;
     margin: 20px auto;
---
+++ ninja-gaming/src/routes/guides/index.svelte
@@ -0,0 +1,22 @@
+<div class="guides">
+  <ul>
+    <li><a href="/">guide 1</a></li>
+    <li><a href="/">guide 2</a></li>
+  </ul>
+</div>
+
+<style>
+  .guides {
+    margin-top: 20px;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  a {
+    display: inline-block;
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px dotted rgba(255,255,255,0.2);
+  }
+</style>
